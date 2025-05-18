adb5fcedabd250e1c4bfc7cf7f6e6f52581d4ac1
r123456 <88819758+R196126@users.noreply.github.com>
2025-05-18 21:11:54 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'todo-pwa-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './main.js',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match('./');
+    })
+  );
+});
